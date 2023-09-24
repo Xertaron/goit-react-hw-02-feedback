ee9d9626644403695a9b8ae58f3ca12acd34c67c
Xertaron <xertaron@gmail.com>
2023-09-24 18:06:54 +0200

    d

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,8 +7,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101',
-        opacity: 1
+        color: '#010101'
       }}
     >
       React homework template
